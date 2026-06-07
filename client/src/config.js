55dcd1b3fee213b7830d27fd777c7361700d21e5
alanaelsner-lgtm <alana.elsner@gmail.com>
2026-06-07 14:41:21 -0500

    update

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,2 @@
-export const API_URL = 'http://localhost:3001';
-export const WS_URL = 'ws://localhost:3001';
+export const API_URL = 'https://antiquer-production-cd24.up.railway.app';
+export const WS_URL = 'wss://antiquer-production-cd24.up.railway.app';
